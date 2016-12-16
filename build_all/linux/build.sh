cfea2b1aff1a9b8a54fbeac9cf0e602453b7dff2
Cesar Chavez <cesar@petrolog.us>
2016-12-15 18:59:24 -0600

    Update build.sh
    removed call to c build.sh

--- build_all/linux/build.sh
+++ build_all/linux/build.sh
@@ -30,10 +30,6 @@ process_args()
 
 process_args $*
 
-# instruct C builder to include python library and to skip tests
-
-./c/build_all/linux/build.sh --build-python $PYTHON_VERSION --skip-unittests $*
-[ $? -eq 0 ] || exit $?
 cd $build_root
 
 echo copy iothub_client library to samples folder
